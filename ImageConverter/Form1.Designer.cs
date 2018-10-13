e83ef6f90dfc8e5f5f0afc1298957597dcf3ff28
unknown <Hackaton@LT411106.asml.com>
2018-10-13 09:30:05 +0200

    send shapes, ui changes

--- ImageConverter/Form1.Designer.cs
+++ ImageConverter/Form1.Designer.cs
@@ -28,98 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tbPath = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.btLoadA = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.btBrowse = new System.Windows.Forms.Button();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.btSend = new System.Windows.Forms.Button();
             this.btLoadB = new System.Windows.Forms.Button();
+            this.btLoadA = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // tbPath
-            // 
-            this.tbPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbPath.Location = new System.Drawing.Point(12, 12);
-            this.tbPath.Name = "tbPath";
-            this.tbPath.Size = new System.Drawing.Size(702, 26);
-            this.tbPath.TabIndex = 1;
-            this.tbPath.Text = "C:\\Users\\Hackaton\\Desktop\\test.bmp";
             // 
             // panel1
             // 
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.Location = new System.Drawing.Point(12, 93);
+            this.panel1.Location = new System.Drawing.Point(16, 176);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(745, 379);
+            this.panel1.Size = new System.Drawing.Size(891, 313);
             this.panel1.TabIndex = 6;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
-            // btLoadA
+            // groupBox1
             // 
-            this.btLoadA.Location = new System.Drawing.Point(12, 51);
-            this.btLoadA.Name = "btLoadA";
-            this.btLoadA.Size = new System.Drawing.Size(75, 36);
-            this.btLoadA.TabIndex = 7;
-            this.btLoadA.Text = "Load A";
-            this.btLoadA.UseVisualStyleBackColor = true;
-            this.btLoadA.Click += new System.EventHandler(this.btLoadA_Click);
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btBrowse);
+            this.groupBox1.Controls.Add(this.tbPath);
+            this.groupBox1.Controls.Add(this.btSend);
+            this.groupBox1.Controls.Add(this.btLoadB);
+            this.groupBox1.Controls.Add(this.btLoadA);
+            this.groupBox1.Location = new System.Drawing.Point(16, 13);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(602, 151);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Set image";
             // 
             // btBrowse
             // 
             this.btBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btBrowse.Location = new System.Drawing.Point(720, 12);
+            this.btBrowse.Location = new System.Drawing.Point(531, 25);
+            this.btBrowse.Margin = new System.Windows.Forms.Padding(4);
             this.btBrowse.Name = "btBrowse";
-            this.btBrowse.Size = new System.Drawing.Size(37, 36);
-            this.btBrowse.TabIndex = 8;
+            this.btBrowse.Size = new System.Drawing.Size(49, 45);
+            this.btBrowse.TabIndex = 15;
             this.btBrowse.Text = "...";
             this.btBrowse.UseVisualStyleBackColor = true;
             this.btBrowse.Click += new System.EventHandler(this.btBrowse_Click);
             // 
-            // openFileDialog1
+            // tbPath
             // 
-            this.openFileDialog1.FileName = "openFileDialog1";
+            this.tbPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbPath.Location = new System.Drawing.Point(19, 31);
+            this.tbPath.Margin = new System.Windows.Forms.Padding(4);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(503, 30);
+            this.tbPath.TabIndex = 14;
+            this.tbPath.Text = "C:\\Users\\Hackaton\\Desktop\\test.bmp";
+            // 
+            // btSend
+            // 
+            this.btSend.Location = new System.Drawing.Point(267, 80);
+            this.btSend.Margin = new System.Windows.Forms.Padding(4);
+            this.btSend.Name = "btSend";
+            this.btSend.Size = new System.Drawing.Size(113, 45);
+            this.btSend.TabIndex = 13;
+            this.btSend.Text = "Send";
+            this.btSend.UseVisualStyleBackColor = true;
+            this.btSend.Click += new System.EventHandler(this.btSend_Click);
             // 
             // btLoadB
             // 
-            this.btLoadB.Location = new System.Drawing.Point(111, 51);
+            this.btLoadB.Location = new System.Drawing.Point(127, 80);
+            this.btLoadB.Margin = new System.Windows.Forms.Padding(4);
             this.btLoadB.Name = "btLoadB";
-            this.btLoadB.Size = new System.Drawing.Size(75, 36);
-            this.btLoadB.TabIndex = 9;
+            this.btLoadB.Size = new System.Drawing.Size(100, 45);
+            this.btLoadB.TabIndex = 12;
             this.btLoadB.Text = "Load B";
             this.btLoadB.UseVisualStyleBackColor = true;
             this.btLoadB.Click += new System.EventHandler(this.btLoadB_Click);
             // 
+            // btLoadA
+            // 
+            this.btLoadA.Location = new System.Drawing.Point(19, 80);
+            this.btLoadA.Margin = new System.Windows.Forms.Padding(4);
+            this.btLoadA.Name = "btLoadA";
+            this.btLoadA.Size = new System.Drawing.Size(100, 45);
+            this.btLoadA.TabIndex = 11;
+            this.btLoadA.Text = "Load A";
+            this.btLoadA.UseVisualStyleBackColor = true;
+            this.btLoadA.Click += new System.EventHandler(this.btLoadA_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Location = new System.Drawing.Point(626, 13);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(280, 151);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Status";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(769, 484);
-            this.Controls.Add(this.btLoadB);
-            this.Controls.Add(this.btBrowse);
-            this.Controls.Add(this.btLoadA);
+            this.ClientSize = new System.Drawing.Size(923, 504);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Image Converter";
+            this.Text = "ASML Hackathon Yello Team";
             this.ResizeBegin += new System.EventHandler(this.Form1_ResizeBegin);
             this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
             this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.TextBox tbPath;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button btLoadA;
+        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button btBrowse;
-        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Button btSend;
         private System.Windows.Forms.Button btLoadB;
+        private System.Windows.Forms.Button btLoadA;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
--- ImageConverter/Form1.cs
+++ ImageConverter/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,6 +14,8 @@ namespace ImageConverter
 {
     public partial class Form1 : Form
     {
+        string API_DOMAIN = "http://10.14.0.205/";
+
         string path;
         Bitmap bitmapA;
         Bitmap bitmapB;
@@ -28,10 +31,10 @@ namespace ImageConverter
             e.Graphics.FillRectangle(brush, 0, 0, panel1.Width, panel1.Height);
             if (bitmapA != null)
             {
-                int side = panel1.Width/2;
+                int side = panel1.Width / 2;
                 if (panel1.Height < side)
                     side = panel1.Height;
-                int left = (panel1.Width/2 - side) / 2;
+                int left = (panel1.Width / 2 - side) / 2;
                 int top = (panel1.Height - side) / 2;
                 Rectangle rect = new Rectangle(left, top, side, side);
                 e.Graphics.DrawImage(bitmapA, rect);
@@ -43,7 +46,7 @@ namespace ImageConverter
                     side = panel1.Height;
                 int left = (panel1.Width / 2 - side) / 2;
                 int top = (panel1.Height - side) / 2;
-                Rectangle rect = new Rectangle(left+panel1.Width/2, top, side, side);
+                Rectangle rect = new Rectangle(left + panel1.Width / 2, top, side, side);
                 e.Graphics.DrawImage(bitmapB, rect);
             }
         }
@@ -62,7 +65,7 @@ namespace ImageConverter
                 int newHeight = 8 * 4;
                 byte[,] scaledPixels = ImageTools.ResizeImageCentered(origPixels, newWidth, newHeight);
                 string text = ImageTools.ConvertToText(scaledPixels);
-                File.WriteAllText(Path.Combine(txtPath,fileName)+" "+barCount+".txt", text);
+                File.WriteAllText(Path.Combine(txtPath, fileName) + " " + barCount + ".txt", text);
             }
         }
 
@@ -100,6 +103,57 @@ namespace ImageConverter
         private void Form1_Resize(object sender, EventArgs e)
         {
             panel1.Invalidate();
+        }
+
+        private async void btSend_Click(object sender, EventArgs e)
+        {
+            string queryFormat = "SHAPES/?A1={0}&A2={1}&A3={2}&A4={3}&B1={4}&B2={5}&B3={6}&B4={7}";
+            byte[,] pixelsA = ImageTools.LoadBitmap(bitmapA);
+            byte[,] pixelsB = ImageTools.LoadBitmap(bitmapB);
+
+            byte[,] scaledPixelsA1 = ImageTools.ResizeImageCentered(pixelsA, 8 * 1, 8 * 4);
+            string textA1 = ImageTools.ConvertToText(scaledPixelsA1).Replace("\r\n", "");
+
+            byte[,] scaledPixelsA2 = ImageTools.ResizeImageCentered(pixelsA, 8 * 2, 8 * 4);
+            string textA2 = ImageTools.ConvertToText(scaledPixelsA1).Replace("\r\n", "");
+
+            byte[,] scaledPixelsA3 = ImageTools.ResizeImageCentered(pixelsA, 8 * 3, 8 * 4);
+            string textA3 = ImageTools.ConvertToText(scaledPixelsA1).Replace("\r\n", "");
+
+            byte[,] scaledPixelsA4 = ImageTools.ResizeImageCentered(pixelsA, 8 * 4, 8 * 4);
+            string textA4 = ImageTools.ConvertToText(scaledPixelsA1).Replace("\r\n", "");
+
+            byte[,] scaledPixelsB1 = ImageTools.ResizeImageCentered(pixelsB, 8 * 1, 8 * 4);
+            string textB1 = ImageTools.ConvertToText(scaledPixelsB1).Replace("\r\n", "");
+
+            byte[,] scaledPixelsB2 = ImageTools.ResizeImageCentered(pixelsB, 8 * 2, 8 * 4);
+            string textB2 = ImageTools.ConvertToText(scaledPixelsB1).Replace("\r\n", "");
+
+            byte[,] scaledPixelsB3 = ImageTools.ResizeImageCentered(pixelsB, 8 * 3, 8 * 4);
+            string textB3 = ImageTools.ConvertToText(scaledPixelsB1).Replace("\r\n", "");
+
+            byte[,] scaledPixelsB4 = ImageTools.ResizeImageCentered(pixelsB, 8 * 4, 8 * 4);
+            string textB4 = ImageTools.ConvertToText(scaledPixelsB1).Replace("\r\n", "");
+
+            string queryStr = String.Format(queryFormat, 
+                textA1, textA2, textA3, textA4,
+                textB1, textB2, textB3, textB4);
+
+            using (HttpClient client = new HttpClient())
+            using (HttpResponseMessage response = await client.GetAsync(API_DOMAIN+queryStr))
+            using (HttpContent content = response.Content)
+            {
+                string result = await content.ReadAsStringAsync();
+
+                if (result != null && result.Length >= 0)
+                {
+                    MessageBox.Show(result);
+                }
+                else
+                {
+                    MessageBox.Show("Error! Empty result");
+                }
+            }
         }
     }
 }
